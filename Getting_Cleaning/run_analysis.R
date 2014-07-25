d7c04423821bfa7d223348df6e0f7a08bf1ee299
Frankie Ragnet <Francois.Ragnet@xerox.com>
2014-07-25 18:59:34 +0200

    better description in header

--- Getting_Cleaning/run_analysis.R
+++ Getting_Cleaning/run_analysis.R
@@ -1,4 +1,12 @@
 ##This function opens the various datasets, then merges them into a single data set
+## Read dataset formatted like this one:
+##  https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+## then:
+## - Merges the training and the test sets to create one data set.
+## - Extracts only the measurements on the mean and standard deviation for each measurement. 
+## - Uses descriptive activity names to name the activities in the data set
+## - Appropriately labels the data set with descriptive variable names. 
+## - Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
 
 run_analysis<-function()
 {
